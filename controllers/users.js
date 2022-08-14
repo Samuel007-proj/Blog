77469490372a93f6a4fe8d4faecc885e61681e53
Samuel Oyelami <oyelamisamuel31@gmail.com>
2022-08-14 20:46:28 +0100

    new user crashes fixes

--- controllers/users.js
+++ controllers/users.js
@@ -33,30 +33,32 @@ usersRouter.post('/api/users', async (req, resp) => {
 usersRouter.get('/api/user/:username', async (req, resp) => {
     const username = req.params.username
     const user =  await User.findOne({username: username})
-    const userId = await user._id
-
-    const blogs = await Blog.find({user: userId})
-
-    const getLikes = async blogs => {
-        let likes = []
-        await blogs.forEach( blog => {
-             likes.push(blog.likes)
-        } )
-        return likes
-    }
-
-    const likesArray = await getLikes(blogs)
-    const totalLikes = likesArray.reduce((sum, prev) => {
-        return sum + prev
-    }, 0 )
-
-    const mostBlogLikes = Math.max(...likesArray)
-
-    const mostLikedBlog = await Blog.find({ likes: mostBlogLikes })
-
+    
+    
     if(user){
+        const userId = await user._id
+
+        const blogs = await Blog.find({user: userId})
+
         const blogsLength = blogs.length
         if(blogsLength){
+            const getLikes = async blogs => {
+                let likes = []
+                await blogs.forEach( blog => {
+                     likes.push(blog.likes)
+                } )
+                return likes
+            }
+        
+            const likesArray = await getLikes(blogs)
+            const totalLikes = likesArray.reduce((sum, prev) => {
+                return sum + prev
+            }, 0 )
+        
+            const mostBlogLikes = Math.max(...likesArray)
+        
+            const mostLikedBlog = await Blog.find({ likes: mostBlogLikes })
+        
             const mostLikedBlogTitle = await mostLikedBlog[0].title
             resp.json({blogsLength, totalLikes, mostLikedBlogTitle})
         } else {
